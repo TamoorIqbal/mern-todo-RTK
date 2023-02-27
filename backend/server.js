4059cc217e707d36ef45f6b88b0e997b3428eb45
TamoorIqbal <tamooriqbal37@gmail.com>
2023-02-27 15:59:14 +0500

    vercel.json

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,13 @@
 const dotenv = require("dotenv");
 const app = require("./app");
 
-
 const connectDatabase = require("./config/database");
 
-dotenv.config({ path:"./config/config.env"});
+dotenv.config({ path: "./config/config.env" });
 
 connectDatabase();
 
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log(`Server is running on port: ${process.env.PORT} `);
 });
 //   process.env.PORT

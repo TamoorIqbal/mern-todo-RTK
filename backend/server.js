adf03850e8892dde3f15ff785f44a1c4bd7e9bf0
TamoorIqbal <tamooriqbal37@gmail.com>
2023-02-28 11:01:59 +0500

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv");
 const app = require("./app.js");
+const path = require("path");
 
 const connectDatabase = require("./config/database.js");
 
@@ -11,7 +12,18 @@ app.listen(process.env.PORT || 4000, () => {
   console.log(`Server is running on port: ${process.env.PORT} `);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello world!");
+// });
+if (process.env.PORT === "production") {
+  app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'),function (err) {
+          if(err) {
+              res.status(500).send(err)
+          }
+      });
+  })
+
+}
 

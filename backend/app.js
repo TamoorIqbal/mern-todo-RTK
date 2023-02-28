ac422ecb214a58603569082854281a442738d038
TamoorIqbal <tamooriqbal37@gmail.com>
2023-02-28 10:22:42 +0500

    first commit

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,6 @@ app.use("/api/v1", todo);
 
 
 if (process.env.NODE_ENV === "production") {
-  const path = require("path");
   app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
   app.get("*", (req, res) => {
       res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'),function (err) {

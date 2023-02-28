be00a5eecc8981da624fe7679e67dec05be2b5d6
TamoorIqbal <tamooriqbal37@gmail.com>
2023-02-28 12:01:42 +0500

    first commit

--- backend/app.js
+++ backend/app.js
@@ -11,30 +11,24 @@ app.use(cors());
 const todo = require("./route/TodoRoute.js");
 app.use("/api/v1", todo);
 
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
-//   app.get("*", (req, res) => {
-//       res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'),function (err) {
-//           if(err) {
-//               res.status(500).send(err)
-//           }
-//       });
-//   })
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
-
+  //   app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
+  //   app.get("*", (req, res) => {
+  //       res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'),function (err) {
+  //           if(err) {
+  //               res.status(500).send(err)
+  //           }
+  //       });
+  //   })
+  app.get("/", (req, res) => {
+    app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
 }
 
 // app.get("/", (req, res) => {
 //   res.send("Hello world!");
 // });
-
-
-
-
-
 
 // app.use(express.static(path.resolve("./frontend/build")));
 // app.get("*", (req, res) => {

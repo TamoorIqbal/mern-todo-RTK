99a94bea8ddcd1e96f1ae6f671e0a1397ef9408b
TamoorIqbal <tamooriqbal37@gmail.com>
2023-02-28 12:31:11 +0500

    first commit

--- frontend/src/store/todoSlice.js
+++ frontend/src/store/todoSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const todoApi = createApi({
   reducerPath: "todoApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://mern-todo-rtk.vercel.app/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://mern-todo-rtk-lpt2.vercel.app/api/v1" }),
   endpoints: (builder) => ({
     getAllTodos: builder.query({
       query: () => {
